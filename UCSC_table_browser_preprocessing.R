52cae5b0969ed54a1c1e5c24b67e53b7093b8828
CarolineBao <caroline.yb@gmail.com>
2020-03-02 14:33:07 -0500

    added comments to UCSC_table_browser_preprocessing.R

--- UCSC_table_browser_preprocessing.R
+++ UCSC_table_browser_preprocessing.R
@@ -1,18 +1,21 @@
-#uses annotations from UCSC table browser to produce readable files for 
+#Uses annotations from UCSC table browser to produce readable files for motif matching. Change gene_features' file path, gene_of_interest, gene_name, and the output file path names to match your needs.
 gene_features <- read.table("shank3_annotated_rheMac10_UCSC.txt", header = F, stringsAsFactors = F)
 gene_of_interest <-"XM_028827508.1"
 gene_name<-"Shank3"
+
+#creating the .bed format columns
 gene_features<-gene_features %>%  
                       cbind(.,(.$V5-.$V4), rep(gene_name, nrow(gene_features))) %>%
                       subset(str_detect(V10, gene_of_interest)) %>%
                       .[,c(1,4,5,15,7,3,16)]
 
+#rename the column names to match .bed format
 colnames(gene_features) <- (c("seqnames", "start", "end", "width", "strand", "type", "gene_name"))
 
+#create dataframe for gene file
 gene<-cbind(seqnames=gene_features[1,1], start=gene_features[1,2], end=gene_features[nrow(gene_features),3], 
                 width=gene_features[nrow(gene_features),3]-gene_features[1,2]+1, strand=gene_features[1,5], 
                 type="gene", gene_name=gene_name)
 
 write.table(lapply(as.data.frame(gene_features, stringsAsFactors=F), as.character), paste("output/",gene_name,"_rheMac10_features.txt",sep=''), sep = "\t", quote = F, row.names = F)
 write.table(lapply(as.data.frame(gene, stringsAsFactors=F), as.character), paste("output/",gene_name,"_rheMac10_gene.txt",sep=''), sep = "\t", quote = F, row.names = F)
-
