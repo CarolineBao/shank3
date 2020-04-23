6d4cf930a9810e472dcca44298763d47cd814077
CarolineBao <caroline.yb@gmail.com>
2020-04-23 14:11:40 -0700

    added cytoscape file

--- scripts/Cytoscape_frequencies.R
+++ scripts/Cytoscape_frequencies.R
@@ -3,10 +3,12 @@ library(RCy3)
 cytoscapePing ()
 cytoscapeVersionInfo ()
 
-FILE_PATH<-"data/mouse/UCSC_mm10/"
+FILE_PATH<-"/Users/carolinebao/Documents/UROP/Gene\ Therapy/shank3/data/mouse/UCSC_mm10/"
 THRESHOLD<-10
 freqs <- read.table(paste(FILE_PATH, "results/shank3_tfbs_by_freq_hs_ms_mcm_rr.txt", sep=''), header = T, stringsAsFactors = F)
+print(freqs)
 
+#selects specific nodes by their frequency
 filtered <- subset(freqs, freqs$frequency > THRESHOLD)  
 
 nodes <- data.frame(id=c(filtered$tfbs, 'Shank3'),
@@ -17,4 +19,4 @@ edges <- data.frame(source=c(filtered$tfbs),
                     weight=c(filtered$frequency), # numeric
                     stringsAsFactors=FALSE)
 
-createNetworkFromDataFrames(nodes,edges, title=paste("Shank3 Frequencies Greater Than", THRESHOLD), collection="DataFrame Example")
+createNetworkFromDataFrames(nodes, edges, title=paste("Shank3 Frequencies Greater Than", THRESHOLD), collection="DataFrame Example")
